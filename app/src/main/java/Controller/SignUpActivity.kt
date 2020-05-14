eeaf95e40ab4b9a63665e22025009636cb598c90
Stranger01 <joseantoniosysnet@gmail.com>
2020-05-14 15:05:05 -0500

    added the firebase depencies

--- app/src/main/java/Controller/SignUpActivity.kt
+++ app/src/main/java/Controller/SignUpActivity.kt
@@ -3,6 +3,8 @@ package Controller
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.mysource.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kotlinx.android.synthetic.main.activity_signin.*
@@ -13,10 +15,31 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-       /* btn_sign_up.setOnClickListener {
+        btn_sign_up.setOnClickListener {
 
-            startActivity(Intent(this, SignUpActivity::class.java ))
-            startActivity(intent)
-        }*/
+            CreateAccount()
+        }
+    }
+
+    private fun CreateAccount() {
+
+        val fullName = full_name_signup.text.toString()
+        val userName = username_signup.text.toString()
+        val email = email_signup.text.toString()
+        val password = password_signup.text.toString()
+
+        when{
+            TextUtils.isEmpty(fullName) ->
+                Toast.makeText(this, "Name is required", Toast.LENGTH_LONG).show()
+
+            TextUtils.isEmpty(userName) ->
+                Toast.makeText(this, "Username is required", Toast.LENGTH_LONG).show()
+
+            TextUtils.isEmpty(email) ->
+                Toast.makeText(this, "Email is required", Toast.LENGTH_LONG).show()
+
+            TextUtils.isEmpty(password) ->
+                Toast.makeText(this, "Password is required", Toast.LENGTH_LONG).show()
+        }
     }
 }
