7b13700fd9d27fccc43ffc8f64b3c3336aa475e7
Stranger01 <joseantoniosysnet@gmail.com>
2020-05-14 20:22:44 -0500

    added the firebase instance

--- app/src/main/java/Controller/SignUpActivity.kt
+++ app/src/main/java/Controller/SignUpActivity.kt
@@ -1,15 +1,20 @@
 package Controller
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 import com.mysource.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
-import kotlinx.android.synthetic.main.activity_signin.*
+
 
 class SignUpActivity : AppCompatActivity() {
+
+    private val firebaseAuth: FirebaseAuth? = null
+    private val mDatabase: DatabaseReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +33,7 @@ class SignUpActivity : AppCompatActivity() {
         val email = email_signup.text.toString()
         val password = password_signup.text.toString()
 
-        when{
+        when {
             TextUtils.isEmpty(fullName) ->
                 Toast.makeText(this, "Name is required", Toast.LENGTH_LONG).show()
 
@@ -40,6 +45,36 @@ class SignUpActivity : AppCompatActivity() {
 
             TextUtils.isEmpty(password) ->
                 Toast.makeText(this, "Password is required", Toast.LENGTH_LONG).show()
+
+            else -> {
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Register")
+                progressDialog.setMessage("Pleae wait, this might take a while")
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.show()
+
+                val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            saveUserInfo(fullName, userName, email)
+
+                        } else {
+
+                            val message = task.exception!!.toString()
+                            Toast.makeText(this, "Error$message", Toast.LENGTH_LONG ).show()
+                            progressDialog.dismiss()
+
+                        }
+
+                    }
+            }
+
         }
+    }
+
+    private fun saveUserInfo(fullName: String, userName: String, email: String) {
+            TODO("Not yet implemented")
     }
 }
