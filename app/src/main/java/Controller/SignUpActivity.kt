fbd71180bb51ef9142356e805df7388f03ea554e
Stranger01 <joseantoniosysnet@gmail.com>
2020-05-15 11:38:34 -0500

    added the sign up function, name, username, email and password, created the Db on Firebase, added default profile pic, tested and created new user

--- app/src/main/java/Controller/SignUpActivity.kt
+++ app/src/main/java/Controller/SignUpActivity.kt
@@ -1,17 +1,20 @@
 package Controller
 
-import android.app.ProgressDialog
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.mysource.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 
 class SignUpActivity : AppCompatActivity() {
+
 
     private val firebaseAuth: FirebaseAuth? = null
     private val mDatabase: DatabaseReference? = null
@@ -47,11 +50,6 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.makeText(this, "Password is required", Toast.LENGTH_LONG).show()
 
             else -> {
-                val progressDialog = ProgressDialog(this)
-                progressDialog.setTitle("Register")
-                progressDialog.setMessage("Pleae wait, this might take a while")
-                progressDialog.setCanceledOnTouchOutside(false)
-                progressDialog.show()
 
                 val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
@@ -63,8 +61,8 @@ class SignUpActivity : AppCompatActivity() {
                         } else {
 
                             val message = task.exception!!.toString()
-                            Toast.makeText(this, "Error$message", Toast.LENGTH_LONG ).show()
-                            progressDialog.dismiss()
+                            Toast.makeText(this, "Error$message", Toast.LENGTH_LONG).show()
+                            mAuth.signOut()
 
                         }
 
@@ -75,6 +73,37 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun saveUserInfo(fullName: String, userName: String, email: String) {
-            TODO("Not yet implemented")
+        val currentUserID = FirebaseAuth.getInstance().currentUser!!.uid
+        val userRef: DatabaseReference = FirebaseDatabase.getInstance().reference.child("Users")
+
+        val userMap = HashMap<String, Any>()
+        userMap["uid"] = currentUserID
+        userMap["fullname"] = currentUserID
+        userMap["username"] = currentUserID
+        userMap["email"] = currentUserID
+        userMap["bio"] = "Im using this awesome platform"
+        userMap["image"] =
+            "https://firebasestorage.googleapis.com/v0/b/mysource-2bb9a.appspot.com/o/Default%20Image%2Fprofile.png?alt=media&token=7ff3e996-7b06-4a1a-bc90-3958574ce78e"
+
+        userRef.child(currentUserID).setValue(userMap)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    Toast.makeText(this, "Welcome", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                    finish()
+
+                } else {
+
+                    val mesdage = task.exception!!.toString()
+                    Toast.makeText(this, "Error$mesdage", Toast.LENGTH_LONG).show()
+                    FirebaseAuth.getInstance().signOut()
+
+                }
+
+            }
+
     }
 }
--- app/src/main/java/Controller/SigninActivity.kt
+++ app/src/main/java/Controller/SigninActivity.kt
@@ -3,6 +3,7 @@ package Controller
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.mysource.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kotlinx.android.synthetic.main.activity_signin.*
@@ -16,6 +17,17 @@ class SigninActivity : AppCompatActivity() {
         btn_signup.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java ))
 
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (FirebaseAuth.getInstance().currentUser !=null){
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
         }
     }
 }
